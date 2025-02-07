207e23d4c3647e1372baffb7bc909dd1c09b0379
Colin Ryan <colin.ryan.ga+git@gmail.com>
2025-02-07 08:26:03 -0500

    changed compiled cmd again im not sure whats going on

--- hl-custom-compile.sh
+++ hl-custom-compile.sh
@@ -1,8 +1,13 @@
 mkdir -p .holy-lambda/build
 
 if [[ "$USE_AGENT_CONTEXT" == "true" ]]; then
-  echo "compiling with agent"
-  echo "just skipping"
+  echo "Building Regular JAR..."
+  lein uberjar
+  mkdir -p .holy-lambda/build
+  cp target/output.jar .holy-lambda/build/output.jar
+  cp target/output.jar .holy-lambda/build/output-agent.jar
+  echo "Regular Should be there"
+  ls .holy-lambda/build
 else
   echo "Building Regular JAR..."
   lein uberjar
