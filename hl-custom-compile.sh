def2cca24100ce1a472049678be849d2146f6ce3
Colin Ryan <colin.ryan.ga+git@gmail.com>
2025-02-06 21:40:48 -0500

    added script

--- hl-custom-compile.sh
+++ hl-custom-compile.sh
@@ -3,9 +3,11 @@ mkdir -p .holy-lambda/build
 if [[ "$USE_AGENT_CONTEXT" == "true" ]]; then
   echo "Building Agent JAR..."
   lein uberjar
+  mkdir -p .holy-lambda/build
   cp target/output.jar .holy-lambda/build/output-agent.jar
 else
   echo "Building Regular JAR..."
   lein uberjar
+  mkdir -p .holy-lambda/build
   cp target/output.jar .holy-lambda/build/output.jar
 fi
