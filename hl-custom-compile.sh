c3bd029534c74419afbded9c10e46753c592b117
Colin Ryan <colin.ryan.ga+git@gmail.com>
2025-02-08 08:55:33 -0500

    idk

--- hl-custom-compile.sh
+++ hl-custom-compile.sh
@@ -5,7 +5,6 @@ if [[ "$USE_AGENT_CONTEXT" == "true" ]]; then
   lein uberjar
   mkdir -p .holy-lambda/build
   cp target/output.jar .holy-lambda/build/output.jar
-  cp target/output.jar .holy-lambda/build/output-agent.jar
   echo "Regular Should be there"
   ls .holy-lambda/build
 else
