5387d200dbf0549678db00e528b1a7af243b66b4
Colin Ryan <colin.ryan.ga+git@gmail.com>
2025-02-09 09:20:28 -0500

    aws sdk v2

--- project.clj
+++ project.clj
@@ -23,7 +23,7 @@
                  [io.github.FieryCod/holy-lambda-ring-adapter "0.1.2"]
                  [com.datomic/client-cloud "1.0.130"]
                  [com.datomic/local "1.0.285"]
-                 [software.amazon.awssdk/core "2.30.16"]
+                 [software.amazon.awssdk "2.30.16"]
                  [software.amazon.awssdk/ssm "2.30.16"]
                  [software.amazon.awssdk/kms "2.30.16"]
                  [com.github.clj-easy/graal-build-time "1.0.5"]]
