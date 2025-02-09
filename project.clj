313a02df2a82450cab80e38a96e61624da9e7d66
Colin Ryan <colin.ryan.ga+git@gmail.com>
2025-02-09 09:44:03 -0500

    explicitly exclud slf4j

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "localhost:3000"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :managed-dependencies [[org.slf4j/slf4j-api nil]]
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/clojurescript "1.11.60"]
                  [com.google.javascript/closure-compiler-unshaded "v20220301"]
